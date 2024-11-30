7cb638117d0af8e249f68d1b264d283f851d4743
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-11-30 15:08:37 +0300

    m_13_2.py

---
+++ m_13_2.py
@@ -0,0 +1,25 @@
+# telegram bot
+# Хендлеры обработки сообщений
+import asyncio
+from aiogram import Bot, Dispatcher, types,executor, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = "key"
+bot = Bot(token = api)
+dp =Dispatcher(bot, storage = MemoryStorage())
+
+
+@dp.message_handler(text = ['ff'])
+async def bot_message(message):
+    print('=Some message=')
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print('Привет! Я бот помогающий твоему здоровью ')
+
+@dp.message_handler()
+async  def all_message(message):
+    print('Введите команду /start, чтобы начать общение')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
