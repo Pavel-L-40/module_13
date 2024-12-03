4d04748b839b1d1af0e8012a535a98c574d19505
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2024-12-03 10:51:12 +0300

    Задача "Меньше текста, больше кликов" KeyboardButton

--- m_13_5.py
+++ m_13_5.py
@@ -3,6 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import  State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
 api = "Token"                               <<<=== введите api <<<===
 bot = Bot(token = api)
 dp =Dispatcher(bot, storage = MemoryStorage())
